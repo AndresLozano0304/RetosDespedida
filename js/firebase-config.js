81732c0577990a2381c97d19388d8709d3a333fb
Andrés Lozano Ramos <andres.lozano@interna.canagrosa.com>
2026-07-23 12:39:39 +0200

    conf firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,12 +2,20 @@
 // Pasos para obtenerlos: ver README.md, seccion "Configurar Firebase".
 // Estas claves son publicas por diseno (no son secretas), pero las reglas
 // de la base de datos son las que realmente protegen los datos.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  databaseURL: "https://TU_PROYECTO-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "TU_APP_ID",
+  apiKey: "AIzaSyAUEswMABqnoJFB2hXXcZNfOYtRovmIvs0",
+  authDomain: "retos-sayago.firebaseapp.com",
+  projectId: "retos-sayago",
+  storageBucket: "retos-sayago.firebasestorage.app",
+  messagingSenderId: "176732657519",
+  appId: "1:176732657519:web:951bea1509c98818ebfa9a"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
